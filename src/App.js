ef2619d7fa1cac2ce4c2970120ebedff6490b1b8
Robert Pliszka <robplisz@gmail.com>
2022-07-11 18:21:33 -0400

    Wrote tests for the Gallery and fixed Nav tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import "./App.css";
-import About from "./components/About";
 import Nav from "./components/Nav";
+import About from "./components/About";
 import Gallery from "./components/Gallery";
 
 function App() {
@@ -11,20 +10,16 @@ function App() {
       description:
         "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    {
-      name: "portraits",
-      description: "Portraits of people in my life",
-    },
-    {
-      name: "food",
-      description: "Declicious taste",
-    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
     {
       name: "landscape",
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ]);
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
       <Nav
@@ -33,7 +28,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
